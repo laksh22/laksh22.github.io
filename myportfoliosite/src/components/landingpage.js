ff3af4b20b70e59f1aeef1c9e96cb4953d133868
unknown <lakshyajeet22@gmail.com>
2018-09-14 00:41:40 +0800

    Add social links

--- myportfoliosite/src/components/landingpage.js
+++ myportfoliosite/src/components/landingpage.js
@@ -7,16 +7,35 @@ class LandingPage extends Component {
       <div style={{width: '100%', margin: 'auto'}}>
         <Grid className='landing-grid'>
           <Cell col={12}>
+            {/*Avatar Image*/}
             <img
               src="https://media.licdn.com/dms/image/C5103AQH9Rz9MwQV79g/profile-displayphoto-shrink_200_200/0?e=1542240000&v=beta&t=qEG-kjICyL1vKB3zFEMojatSdYozcGfSfXIGd9tIUDc"
               alt="avatar"
               className="avatar-image"
             />
+
+            {/*Banner containing skills*/}
             <div className="banner-text">
               <h1>Web Developer | App Developer</h1>
               <hr/>
               <p>Flutter | HTML | CSS | JavaScript | Python | React</p>
+
+
+              <div className="social-links">
+
+                {/*Linkedin*/}
+                <a href="http://google.com" target="_blank" rel="noopener noreferrer" >
+                  <i className="fa fa-linkedin-square" aria-hidden="true" />
+                </a>
+
+                {/*Github*/}
+                <a href="http://google.com" target="_blank" rel="noopener noreferrer" >
+                  <i className="fa fa-github-square" aria-hidden="true" />
+                </a>
+
+              </div>
             </div>
+
           </Cell>
         </Grid>
       </div>
