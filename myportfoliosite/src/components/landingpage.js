bf2c6b013dd9e1a27f38fef091a8bc7b69fbdade
unknown <lakshyajeet22@gmail.com>
2018-09-13 17:35:13 +0800

    Add Header and Avatar Image

--- myportfoliosite/src/components/landingpage.js
+++ myportfoliosite/src/components/landingpage.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import {Grid, Cell} from 'react-mdl';
 
 class LandingPage extends Component {
   render(){
     return(
-      <div>
-        <h1>Home</h1>
+      <div style={{width: '100%', margin: 'auto'}}>
+        <Grid className='landing-grid'>
+          <Cell col={12}>
+            <img
+              src="https://media.licdn.com/dms/image/C5103AQH9Rz9MwQV79g/profile-displayphoto-shrink_200_200/0?e=1542240000&v=beta&t=qEG-kjICyL1vKB3zFEMojatSdYozcGfSfXIGd9tIUDc"
+              alt="avatar"
+              className="avatar-image"
+            />
+          </Cell>
+        </Grid>
       </div>
     );
   }
