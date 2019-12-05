8a911772b4304e157e27f44b317e896a21433254
Laksh <lakshyajeet22@gmail.com>
2019-12-05 21:55:33 +0530

    Add main section, sidebar, and card

---
+++ portfolio-site/src/components/Card/Card.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Card.css';
+
+class Card extends Component {
+  render() {
+    return (
+      <div class="card">
+        <div>This is some sample text</div>
+        <div>This is some more sample text</div>
+      </div>
+    );
+  }
+}
+
+export default Card;
---
+++ portfolio-site/src/components/main-section/MainSection.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './MainSection.css';
+import Card from '../Card/Card';
+
+class MainSection extends Component {
+  render() {
+    return (
+      <div id="main-section">
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+        <Card></Card>
+      </div>
+    );
+  }
+}
+
+export default MainSection;
