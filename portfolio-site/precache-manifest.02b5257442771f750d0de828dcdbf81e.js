0369f07e53735e9e592239ddfa03153c513784c7
Laksh <lakshyajeet22@gmail.com>
2019-12-06 14:40:53 +0530

    Build site

---
+++ portfolio-site/precache-manifest.02b5257442771f750d0de828dcdbf81e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "958a22a425075b841104ec51a51f8a4f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1aaf886b10f87e0254d",
+    "url": "/static/css/main.9ee972cd.chunk.css"
+  },
+  {
+    "revision": "b7043b2793b7268dfbdf",
+    "url": "/static/js/2.51eb42b2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.51eb42b2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b1aaf886b10f87e0254d",
+    "url": "/static/js/main.fa70e98c.chunk.js"
+  },
+  {
+    "revision": "fb73f47ce0e63ddb9fbd",
+    "url": "/static/js/runtime-main.dd550126.js"
+  },
+  {
+    "revision": "26e124372b33ad71cd6eefa6e2c11577",
+    "url": "/static/media/ntu_logo.26e12437.png"
+  },
+  {
+    "revision": "e560b7b8f8a597fe58ca088176135ca7",
+    "url": "/static/media/profile-pic.e560b7b8.jpg"
+  }
+]);
