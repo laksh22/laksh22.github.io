c7d5fdc5597056a0ac9cf92a9d4ee53a0c4da397
laksh22 <lakshyajeet22@gmail.com>
2021-05-11 22:14:05 +0800

    Updates

---
+++ precache-manifest.52b4833a665b724ddbc07aecc946b875.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bf1fec300c1f8dd18236d0a54bcc85e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45975c4068fe35c6cb74",
+    "url": "/static/css/main.3bf1e8f1.chunk.css"
+  },
+  {
+    "revision": "c8a6f58462b05de0198e",
+    "url": "/static/js/2.30e194f5.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.30e194f5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "45975c4068fe35c6cb74",
+    "url": "/static/js/main.599dab47.chunk.js"
+  },
+  {
+    "revision": "fb73f47ce0e63ddb9fbd",
+    "url": "/static/js/runtime-main.dd550126.js"
+  },
+  {
+    "revision": "0e89fe001da31ab081f001c561fdb1ee",
+    "url": "/static/media/gsoc.0e89fe00.png"
+  },
+  {
+    "revision": "0158369fe4891d37dd13a30ed9834d38",
+    "url": "/static/media/ntu_full_logo.0158369f.jpg"
+  },
+  {
+    "revision": "e560b7b8f8a597fe58ca088176135ca7",
+    "url": "/static/media/profile-pic.e560b7b8.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0edbb75c8b506cfa99503fa7811529a8.js"
+  "/precache-manifest.52b4833a665b724ddbc07aecc946b875.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
